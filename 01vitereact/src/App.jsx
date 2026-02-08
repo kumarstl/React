b9d9fc95df0dc3c58ff919034e0e6e4b5646988d
sourav kumar <souravkr.stl@gmail.com>
2026-02-09 01:16:58 +0530

    custom React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -4,10 +4,13 @@ import Sourav from"./script";
 
 
 function App(){
+  const username=" Hello Kumar";
+
+
   return (
     <>
     <Sourav/>
-    <h1>Hey i ma sourav</h1>
+    <h1>Hey i ma sourav{username}</h1>
     <p>Test...</p>
     </>
     
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,47 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+
+
+function MYApp(){
+    return(
+        <div>
+            <h2>Custom App !!</h2>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit Google'
+// }
+
+// const anotherElement=(
+//    < a href="https://www.google.com" target="_blank">Visit Google</a>
+// )
+
+
+const anotherUser=" java Script"
+
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://www.google.com',
+    target: '_blank'
+  },
+  'Click me to visit Google',
+  anotherUser
+)
+
 createRoot(document.getElementById('root')).render(
   
-    <App/>
+    reactElement
   
 )
