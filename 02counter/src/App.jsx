676e89042405fa2548bb037e74ba33f98cecae09
sourav kumar <souravkr.stl@gmail.com>
2026-02-09 02:17:56 +0530

    Counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter]=useState(10)
+
+  // let counter = 20
+
+  const addvalue=()=>{
+    // counter=counter+1
+    setCounter(counter+1)    
+    // console.log("Clicked",counter);
+  }
+
+   const removevalue = () => {
+    if (counter > 0) {
+      setCounter(prev => prev - 1)
+    }
+  }
+
+  return (
+    <>
+    <h1>Hey-Buddy !!</h1>
+    <h2>Counter Value :{counter}</h2>
+
+    <button
+     onClick={addvalue}
+     >Add value{counter}</button>
+    <br/>
+    <button
+    onClick={removevalue}
+    >Remove value{counter}</button>
+    <p>Footer:{counter}</p>
+    </>
+    
+  )
+}
+
+export default App
